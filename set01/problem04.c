cb9bf6d7ab8ab7a960e6ee222fa963986cb6f9ae
vaaku <141307898+vaaku@users.noreply.github.com>
2024-01-23 09:59:27 +0530

    test

---
+++ set01/problem04.c
@@ -0,0 +1,5 @@
+//Write a C program to add two numbers using **pass by reference**
+//#include<stdio.h>
+//void input(int *a, int *b);
+//void add(int a, int b, int *sum);
+//void output(int a, int b, int sum);
