0ed618a3fb2576e7a6705c02d6a55034cdbfd1d5
vaaku <141307898+vaaku@users.noreply.github.com>
2024-01-28 20:32:37 +0530

    test

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,2 +1,35 @@
 //Write a C program to find the square root of a number.
 #include<stdio.h>
+#include<math.h>
+
+float input()
+{
+    int n;
+    printf("Enter the value to be square rooted ");
+    scanf("%f",&n);
+    return n;
+}
+float square_root(float n)
+{
+    int x=1,y=n/2;
+    float p=0.000001;
+    while(fabs(y-x)>p)
+    {
+        x=y;
+        y=(x+n/x)/2;
+    }
+    return y;
+}
+void output(float n,float sqrroot)
+{
+    printf("The squareroot of %f ",n);
+    printf("is %f",sqrroot);
+}
+int main()
+{
+    float n,sqrroot;
+    n=input();
+    sqrroot=square_root(n);
+    output(n,sqrroot);
+    return 0;
+}
