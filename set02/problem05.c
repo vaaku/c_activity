cb1067dc4c68193c69b05d094257c736018f9f39
vaaku <141307898+vaaku@users.noreply.github.com>
2024-02-15 17:59:36 +0530

    test

--- set02/problem05.c
+++ set02/problem05.c
@@ -1,2 +1,10 @@
   // Write a program to find GCD _(HCF)_ of two numbers.
+  #include<stdio.h>
+  int input()
+  {
+    int num;
+    printf("Enter a number ");
+    scanf("%d",&num);
+    return num;
+  }
   
