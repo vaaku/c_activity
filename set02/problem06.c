41de8dfc5e8cbb2be034feba8e5c963bdca65779
vaaku <141307898+vaaku@users.noreply.github.com>
2024-02-28 22:17:50 +0530

    test

--- set02/problem06.c
+++ set02/problem06.c
@@ -1,1 +1,6 @@
 // Write a program to find the triangle with smallest area in n given triangles.
+void input_string(char *a)
+{
+    printf("Enter the value of the string ");
+    scanf("%d",&a);
+}
